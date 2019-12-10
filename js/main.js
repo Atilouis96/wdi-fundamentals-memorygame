a4780c89c543b34bde7700ca856820e717952435
Atilouis96 <58232624+Atilouis96@users.noreply.github.com>
2019-12-10 03:15:36 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+console.log("Up and running!");
+console.log("user fliped" + cardOne);
+console.log("user fliped" + cardThree);
+const cards =["queen", "queen", "king", "king"];
+const cardsInPlay[cards.push('cardOne')cards.push("king")];
+var cardOne ("queen");
+var cardTwo ("king");
+if (length cardsInPlay === 2) {
+} else alert("You found a match!") {
+
+}
+
